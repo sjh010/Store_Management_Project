964d556e6bc7c5e601c7ea2fc80f4524a4482266
sjh010 <sjh010@naver.com>
2014-03-11 21:50:27 +0900

    Create UpdateUI.java

---
+++ store.view/UpdateUI.java
@@ -0,0 +1,18 @@
+package store.view;
+
+import store.service.StoreService;
+
+public class UpdateUI extends AbstractUI{ // 아직 구현 못함.
+
+	public UpdateUI(StoreService service) {
+		super(service);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doJob() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
